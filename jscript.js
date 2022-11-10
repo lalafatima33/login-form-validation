6793bc6ec4209a3a2bd9426e9dbe7a0d9fea1cb5
lalafatima33 <82950609+lalafatima33@users.noreply.github.com>
2022-11-10 20:31:35 +0100

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,9 @@
+let username = document.getElementById('username').
+value;
+let password = document.getElementById('password').
+value;
+if(username ==='Fatima' && password ==='Bota'){
+    alert('login successfuly!')
+}else {
+    alert ('failed')
+}
